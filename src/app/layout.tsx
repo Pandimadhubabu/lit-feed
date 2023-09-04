d0dc185aaa6de486fac33dfd9b9df138cbcb589c
Amin Marashi <me@amin.codes>
2023-09-05 00:00:00 +0200

    feat: add app shell using nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Yafr!',
+  description: 'Yet another feed reader',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full bg-gray-900">
+      <body className={`h-full ${inter.className}`}>{children}</body>
+    </html>
+  )
+}
