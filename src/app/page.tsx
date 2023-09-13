937fd4bf7efdf2964ea1c2f3791ed497a0624a8c
Amin Marashi <me@amin.codes>
2023-09-14 00:00:00 +0200

    fix: header colors in light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
       <div className="xl:pl-72">
         {/* Sticky search header */}
         <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-6 border-b border-white/5 bg-gray-900 px-4 shadow-sm sm:px-6 lg:px-8">
-          <button type="button" className="-m-2.5 p-2.5 dark:text-white text-black xl:hidden" onClick={() => setSidebarOpen(true)}>
+          <button type="button" className="-m-2.5 p-2.5 text-white xl:hidden" onClick={() => setSidebarOpen(true)}>
             <span className="sr-only">Open sidebar</span>
             <Bars3Icon className="h-5 w-5" aria-hidden="true" />
           </button>
@@ -139,12 +139,12 @@ export default function Home() {
               </label>
               <div className="relative w-full">
                 <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 dark:text-gray-500 text-gray-800"
+                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-500"
                   aria-hidden="true"
                 />
                 <input
                   id="search-field"
-                  className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 dark:text-white text-black focus:ring-0 sm:text-sm"
+                  className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-white focus:ring-0 sm:text-sm"
                   placeholder="Search..."
                   type="search"
                   name="search"
