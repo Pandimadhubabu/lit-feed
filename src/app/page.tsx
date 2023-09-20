82527c3779530d8f8bd94c7bb8d3cc51c9207991
Amin Marashi <me@amin.codes>
2023-09-21 00:00:00 +0200

    feat: remove search and feed sort features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,7 @@ const deployments = [
   environment: keyof typeof environments
   [key: string]: string | number
 }[]
+
 const articles = [
   {
     title: {
@@ -184,91 +185,13 @@ export default function Home() {
             <Bars3Icon className="h-5 w-5" aria-hidden="true" />
           </button>
 
-          <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-            <form className="flex flex-1" action="#" method="GET">
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full">
-                <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-500"
-                  aria-hidden="true"
-                />
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-white focus:ring-0 sm:text-sm"
-                  placeholder="Search..."
-                  type="search"
-                  name="search"
-                />
-              </div>
-            </form>
-          </div>
+          <header className="flex items-center justify-between border-b border-white/5 w-full px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
+            <h2 className="text-base font-semibold leading-7 dark:text-white text-black">All feeds</h2>
+          </header>
         </div>
 
         {/* Feed list */}
         <aside className="bg-gray-800 lg:fixed lg:bottom-0 lg:left-0 lg:top-16 lg:w-96 lg:overflow-y-auto lg:border-l lg:border-white/5 xl:left-auto">
-          <header className="flex items-center justify-between border-b border-white/5 px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
-            <h2 className="text-base font-semibold leading-7 dark:text-white text-black">All feeds</h2>
-            {/* Sort dropdown */}
-            <Menu as="div" className="relative">
-              <Menu.Button className="flex items-center gap-x-1 text-sm font-medium leading-6 dark:text-white text-black">
-                Sort by
-                <ChevronUpDownIcon className="h-5 w-5 dark:text-gray-500 text-gray-800" aria-hidden="true" />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 z-10 mt-2.5 w-40 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 dark:text-gray-900 text-gray-400'
-                        )}
-                      >
-                        Most recent first
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 dark:text-gray-900 text-gray-400'
-                        )}
-                      >
-                        Most popular first
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 dark:text-gray-900 text-gray-400'
-                        )}
-                      >
-                        Things I might like
-                      </a>
-                    )}
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          </header>
           <ul role="list" className="divide-y divide-white/5">
             {articles.map((item, index) => (
               <li key={item.link} className="px-4 py-4 sm:px-6 lg:px-8" onClick={() => { setArticleOpen(true); setSelectedArticle(index) }}>
