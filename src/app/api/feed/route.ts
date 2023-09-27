40dd2fdf5fa330e5d51aa0ebbb6027e8e9e9b909
Amin Marashi <me@amin.codes>
2023-09-28 00:00:00 +0200

    fix: route request type

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -1,6 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from 'mongodb';
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 if (!process.env.MONGO_URL) {
   throw new Error('Please define the MONGO_URL environment variable inside .env.local')
@@ -13,7 +12,7 @@ const client = new MongoClient(process.env.MONGO_URL);
  * @param request Ignored
  * @returns A list of all feed sources
  */
-export async function GET(request: NextApiRequest) {
+export async function GET(request: NextRequest) {
   await client.connect();
   const database = client.db('feed');
   const collection = database.collection('sources');
