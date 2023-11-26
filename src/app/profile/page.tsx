b23c17c44a7013710d099aaefc4be6a183c6f875
Amin Marashi <me@amin.codes>
2023-11-26 00:00:00 +0000

    chore: add support for gravatar

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,35 +1,64 @@
 "use client";
+import { Loading } from "@/components/Loading";
 import Shell from "@/components/Shell";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
 export default function Profile() {
   const { user, isLoading: isUserLoading, error: userError } = useUser();
+  if (isUserLoading) {
+    return (
+      <Shell headerTitle="Profile">
+        <main className="w-full h-full">
+          <Loading />
+        </main>
+      </Shell>
+    );
+  }
+
+  if (userError) {
+    return (
+      <Shell headerTitle="Profile">
+        <main className="w-full h-full">
+          <div>{userError.message}</div>
+        </main>
+      </Shell>
+    );
+  }
+
+  if (!user) {
+    return (
+      <Shell headerTitle="Profile">
+        <main className="w-full h-full">
+          <div>Not logged in</div>
+        </main>
+      </Shell>
+    );
+  }
   return (
     <Shell headerTitle="Profile">
       <main className="w-full h-full">
-        {isUserLoading || !user ? (
-          "Loading..."
-        ) : userError ? (
-          <div>{userError.message}</div>
-        ) : (
-          <div className="flex flex-col items-center">
-            <img
+        <div className="flex flex-col items-center">
+          {user.picture && (
+            <Image
               src={user.picture}
-              alt={user.name}
-              className="w-60 h-60 rounded-full p-4"
+              alt={user.name ?? ""}
+              width={120}
+              height={120}
+              className="rounded-full p-4"
             />
-            <div className="p-4">{user.name}</div>
-            <div className="py-2 px-4">{user.email}</div>
-            <button
-              onClick={() => {
-                window.location.href = "/api/auth/logout";
-              }}
-              className="bg-gray-800 text-white px-4 py-2 m-4 rounded-md"
-            >
-              Logout
-            </button>
-          </div>
-        )}
+          )}
+          <div className="p-4">{user.name}</div>
+          <div className="py-2 px-4">{user.email}</div>
+          <button
+            onClick={() => {
+              window.location.href = "/api/auth/logout";
+            }}
+            className="bg-gray-800 text-white px-4 py-2 m-4 rounded-md"
+          >
+            Logout
+          </button>
+        </div>
       </main>
     </Shell>
   );
