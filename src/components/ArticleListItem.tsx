63704e0dfa5c7c04dfb2d975661050b5156bd63a
Amin Marashi <me@amin.codes>
2023-12-20 00:00:00 +0000

    fix: avoid overflowing tiny screens

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -17,19 +17,19 @@ export function ArticleListItem({
       } px-4 py-4 sm:px-6 lg:px-8 cursor-pointer`}
     >
       <div className="flex items-center flex-col gap-x-3">
-        <h3 className="dark:text-white text-black flex-auto w-full truncate text-sm font-semibold leading-6">
+        <h3 className="dark:text-white text-black flex-auto text-sm font-semibold leading-6">
           {article.title}
         </h3>
         {!!article.summary && (
           <p
             id="summary"
-            className="dark:text-gray-400 text-gray-800 flex-auto w-full truncate text-xs"
+            className="dark:text-gray-400 text-gray-800 flex-auto text-xs"
           >
             {article.summary}
           </p>
         )}
       </div>
-      <p className="dark:text-gray-500 text-gray-800 mt-3 truncate text-sm">
+      <p className="dark:text-gray-500 text-gray-800 mt-3 text-sm">
         Published on{" "}
         <span className="dark:text-gray-400 text-gray-800">
           {article.feedName}
