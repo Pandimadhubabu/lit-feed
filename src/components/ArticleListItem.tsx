b3191aa7485da338caadf8d2cef0e1b74d7ed27e
Amin Marashi <me@amin.codes>
2023-12-20 00:00:00 +0000

    fix: article item width in feed

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -16,17 +16,16 @@ export function ArticleListItem({
           : "dark:bg-gray-800 bg-gray-100 hover:bg-gray-700"
       } px-4 py-4 sm:px-6 lg:px-8 cursor-pointer`}
     >
-      <div className="flex items-center flex-col gap-x-3">
+      <div className="flex flex-col gap-x-3">
         <h3 className="dark:text-white text-black flex-auto text-sm font-semibold leading-6">
           {article.title}
         </h3>
         {!!article.summary && (
           <p
             id="summary"
-            className="dark:text-gray-400 text-gray-800 flex-auto text-xs"
-          >
-            {article.summary}
-          </p>
+            className="dark:text-gray-400 text-gray-800 flex-auto text-xs h-12 overflow-hidden"
+            dangerouslySetInnerHTML={{ __html: article.summary }}
+          ></p>
         )}
       </div>
       <p className="dark:text-gray-500 text-gray-800 mt-3 text-sm">
